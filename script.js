083df8c891e1e6c0504559ff90e1e5f9a74223d4
AmAcreep <sorngmini46@gmail.com>
2025-01-19 16:23:24 +0700

    Update script.js 2

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ var description=document.querySelector("#description");
 var btnyes=document.querySelector("#btnyes");
 var btnno=document.querySelector("#btnno");
 var btnreset=document.querySelector('#btnreset');
-var happycat=new Audio("../Image/audio/happy-happy-happy-song.mp3");
-var sadcat=new Audio("../Image/audio/hamster dry drag.mp3");
+var happycat=new Audio("audio/gta-san-andreas-mission-complete-sound-hq.mp3");
+var sadcat=new Audio("audio/wait-wait-wait-what-the-hell-legend-sound.mp3");
 profile.src="photo/Hd_dog_hold_flower.jpg";
 profile.alt="Profile not found";
 title.innerHTML="- Lysorng";
@@ -17,6 +17,7 @@ btnyes.addEventListener("click",()=>{
   profile.src="gif/dancin_baby.gif"
   description.innerHTML="ヾ(≧ ▽ ≦)ゝ"
   sadcat.pause();
+  sadcat.currentTime = 0;
   happycat.play();
 })
 
@@ -24,10 +25,15 @@ btnno.addEventListener("click",()=>{
   profile.src="gif/gif_nono.gif"
   description.innerHTML="(┬┬﹏┬┬)"
   happycat.pause();
+  happycat.currentTime = 0;
   sadcat.play();
 })
 
 btnreset.addEventListener("click",()=>{
   profile.src="photo/Hd_dog_hold_flower.jpg"
   description.innerHTML="Be Mine..?💌"
+  sadcat.pause();
+  sadcat.currentTime = 0;
+  happycat.pause();
+  happycat.currentTime = 0;
 })
